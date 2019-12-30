e7fba727597109f2df8781396f092364a319e8ac
Jude Chinoso <jchinoso@theagromall.com.ng>
2019-12-30 09:53:19 +0100

    Updates

---
+++ precache-manifest.35602331aee9a28fa951e9dffed253d9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c25c14632548358d893822565382213",
+    "url": "/gebc/index.html"
+  },
+  {
+    "revision": "e3206c176163d990b7b1",
+    "url": "/gebc/static/css/main.40b7c04a.chunk.css"
+  },
+  {
+    "revision": "c362a78ed854259b06c5",
+    "url": "/gebc/static/js/2.6658964e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gebc/static/js/2.6658964e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3206c176163d990b7b1",
+    "url": "/gebc/static/js/main.905a7f92.chunk.js"
+  },
+  {
+    "revision": "d54ec91090bae00c6020",
+    "url": "/gebc/static/js/runtime-main.8d7cfff7.js"
+  },
+  {
+    "revision": "91f52a5e75dacc27c0c47afa9e72a343",
+    "url": "/gebc/static/media/Congregation.91f52a5e.jpg"
+  },
+  {
+    "revision": "03dda373e5e097656d56311c3091390d",
+    "url": "/gebc/static/media/Help.03dda373.png"
+  },
+  {
+    "revision": "5a5527db5fa8ff0a8b8f4a836d022395",
+    "url": "/gebc/static/media/Image 01.5a5527db.png"
+  },
+  {
+    "revision": "8c984946fdfc1898e327449055524fd2",
+    "url": "/gebc/static/media/Image 02.8c984946.png"
+  },
+  {
+    "revision": "30673003cd9f2a78b00d4d5dfe7f34f6",
+    "url": "/gebc/static/media/Instagram.30673003.png"
+  },
+  {
+    "revision": "dcbd230a90d72fcd055b6561c222af32",
+    "url": "/gebc/static/media/Logo.dcbd230a.png"
+  },
+  {
+    "revision": "ed82370221c18141d92920343f617018",
+    "url": "/gebc/static/media/Man.ed823702.png"
+  },
+  {
+    "revision": "2cc12a6eeac91f2c620647e7da2a7a9c",
+    "url": "/gebc/static/media/Time.2cc12a6e.png"
+  },
+  {
+    "revision": "4653c2149e593d20f76e21a408e945e6",
+    "url": "/gebc/static/media/World.4653c214.png"
+  }
+]);
