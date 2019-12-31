8b64d70cc201be2215fa57df4cdf25db64329e7a
Jude Chinoso <jchinoso@theagromall.com.ng>
2019-12-31 06:04:10 +0100

    finish the blog page and the contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './views/Homepage';
 import About from './views/About';
+import Blog from './views/Blog';
+import Contact from './views/Contact';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -15,6 +17,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/about' component={About} />
+          <Route exact path='/blog' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
           <Route component={HomePage} />
         </Switch>
         <Footer />
